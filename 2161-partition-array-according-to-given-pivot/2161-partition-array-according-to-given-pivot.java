f055c0a2be0fbea3fb849574714f4fd6ce9693cb
dineshgoud7 <116978827+dineshgoud7@users.noreply.github.com>
2026-07-19 10:18:08 +0530

    Time: 5 ms (72.55%), Space: 160 MB (42.47%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,30 +1,21 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        ArrayList<Integer> list1=new ArrayList<>();
-        ArrayList<Integer> list2=new ArrayList<>();
-        ArrayList<Integer> list3=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            if(pivot>nums[i]){
-                list1.add(nums[i]);
-            }else if(pivot==nums[i]){
-                list2.add(nums[i]);
-            }else{
-                list3.add(nums[i]);
-            }
-        }
         int[] arr=new int[nums.length];
         int index=0;
-        for(int i=0;i<list1.size();i++){
-            arr[index]=list1.get(i);
-            index++;
+        for(int num:nums){
+            if(pivot>num){
+                arr[index++]=num;
+            }
         }
-        for(int i=0;i<list2.size();i++){
-            arr[index]=list2.get(i);
-            index++;
+        for(int num:nums){
+            if(pivot==num){
+                arr[index++]=num;
+            }   
         }
-        for(int i=0;i<list3.size();i++){
-            arr[index]=list3.get(i);
-            index++;
+        for(int num:nums){
+            if(pivot<num){
+                arr[index++]=num;
+            }
         }
         return arr;
     }
