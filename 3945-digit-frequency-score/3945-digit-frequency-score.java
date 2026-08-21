484f26f4569c4af792c3d2719c0825d232614d14
dineshgoud7 <116978827+dineshgoud7@users.noreply.github.com>
2026-08-21 06:57:55 +0530

    Time: 1 ms (98.41%), Space: 42.6 MB (70.32%) - LeetHub

---
+++ 3945-digit-frequency-score/3945-digit-frequency-score.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int digitFrequencyScore(int n) {
+        int res=0;
+        while(n!=0){
+            int digit=n%10;
+            res+=digit;
+            n/=10;
+        }
+        return res;
+    }
+}
