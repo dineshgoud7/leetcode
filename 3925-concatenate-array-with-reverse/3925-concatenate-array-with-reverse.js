9ad93429d9eb982d2538da9c910cb9233e8139e0
dineshgoud7 <116978827+dineshgoud7@users.noreply.github.com>
2026-09-12 23:31:45 +0530

    Time: 1 ms (87.69%), Space: 59.6 MB (36.67%) - LeetHub

--- 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.js
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.js
@@ -4,12 +4,9 @@
  */
 var concatWithReverse = function(nums) {
     let arr=[];
-    for(let i=0;i<nums.length;i++){
-        arr.push(nums[i]);
-    }
     for(let i=nums.length-1;i>=0;i--){
         arr.push(nums[i]);
     }
-    return arr;
+    return nums.concat(arr);
     
 };
